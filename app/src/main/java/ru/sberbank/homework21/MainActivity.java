7cd88bcde4e6783207837e69879d3a2b650630ce
Alexander <sapfirmoscow@yandex.ru>
2019-03-15 15:56:36 +0300

    Completed

--- app/src/main/java/ru/sberbank/homework21/MainActivity.java
+++ app/src/main/java/ru/sberbank/homework21/MainActivity.java
@@ -1,13 +1,22 @@
 package ru.sberbank.homework21;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private MyCustomView mMyCustomView;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMyCustomView = findViewById(R.id.myView);
+        mMyCustomView.setOnTouchListener(new MyCutomViewOnDragTouchListener(mMyCustomView));
     }
+
 }
--- app/src/main/java/ru/sberbank/homework21/MyCustomView.java
+++ app/src/main/java/ru/sberbank/homework21/MyCustomView.java
@@ -1,20 +1,24 @@
 package ru.sberbank.homework21;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 public class MyCustomView extends View {
 
-    private int mColor = Color.RED;
+    public static final int DEFAULT_DRAG_COLOR = Color.RED;
+    private final float DEFAULT_SIZE = convertDpToPixel(100);
+    private int userColor;
     private Paint mPaint = new Paint();
-    private int mX;
-    private int mY;
+    private Paint textPaint = new Paint();
+
 
     public MyCustomView(Context context) {
         super(context);
@@ -22,26 +26,77 @@ public class MyCustomView extends View {
 
     public MyCustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        TypedArray typedArray = context.obtainStyledAttributes(attrs,R.styleable.MyCustomView);
-        mColor = typedArray.getColor(R.styleable.MyCustomView_rectColor,Color.GREEN);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MyCustomView);
+
+        //даже если падаем то подчищаем
+        try {
+            userColor = typedArray.getColor(R.styleable.MyCustomView_rectColor, Color.GREEN);
+            mPaint.setColor(userColor);
+        } finally {
+            typedArray.recycle();
+        }
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        final int measuredWidth = reconcileSize(widthMeasureSpec, widthMeasureSpec);
+        final int measuredHeight = reconcileSize(widthMeasureSpec, heightMeasureSpec);
+
+        setMeasuredDimension(measuredWidth, measuredHeight);
+    }
+
+    private int reconcileSize(int contentSize, int measureSpec) {
+        final int mode = MeasureSpec.getMode(measureSpec);
+        final int specSize = MeasureSpec.getSize(measureSpec);
+        switch (mode) {
+            case MeasureSpec.EXACTLY:
+                return specSize;
+            case MeasureSpec.AT_MOST:
+                if (contentSize < specSize) {
+                    return (int) DEFAULT_SIZE;
+                } else {
+                    return specSize;
+                }
+            case MeasureSpec.UNSPECIFIED:
+            default:
+                return contentSize;
+        }
+    }
+
+    private float convertDpToPixel(float dp) {
+        return dp * ((float) getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+    }
+
+    public int getColor() {
+        return userColor;
+    }
+
+    public void setColor(int color) {
+        mPaint.setColor(color);
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int canvasWidth = getMeasuredWidth();
+        int canvasHeight = getMeasuredHeight();
+        @SuppressLint("DrawAllocation") Rect rect = new Rect(0, 0, canvasWidth, canvasHeight);
+        canvas.drawRect(rect, mPaint);
+        drawRectText("x is:" + (int) getX() + " y is " + (int) getY(), canvas, rect);
 
     }
 
+    private void drawRectText(String text, Canvas canvas, Rect r) {
+        textPaint.setTextSize(50);//косяк
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        int width = r.width();
 
-
-    @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
-
+        int numOfChars = textPaint.breakText(text, true, width, null);
+        int start = (text.length() - numOfChars) / 2;
+        canvas.drawText(text, start, start + numOfChars, r.exactCenterX(), r.exactCenterY(), textPaint);
     }
 }
+
+
